5dbc0be3d54799e63839b5275bfb15592bb2445f
Aniket Jain <63732758+AniketJain17@users.noreply.github.com>
2023-01-08 17:05:09 +0530

    Add files via upload

--- migrations/20221126165301-create-user.js
+++ migrations/20221126165301-create-user.js
@@ -2,29 +2,33 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Todos", {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      title: {
+      firstName: {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
           notNull: true,
         },
       },
-      dueDate: {
-        type: Sequelize.DATEONLY,
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
         allowNull: false,
+        unique: true,
         validate: {
           notNull: true,
         },
       },
-      completed: {
-        type: Sequelize.BOOLEAN,
+      password: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
@@ -37,6 +41,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Todos");
+    await queryInterface.dropTable("Users");
   },
 };
